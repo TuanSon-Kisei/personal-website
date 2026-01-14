d7defaef2313af1c9696f97bbea1c518237e28fa
Tuan Son Nguyen <kisei.tuanson.work@gmail.com>
2026-01-14 10:54:11 +0700

    Added p tags for lazy loading images

--- assets/js/lazy-loading-images-not-first.js
+++ assets/js/lazy-loading-images-not-first.js
@@ -1,4 +1,5 @@
 const listItems = document.querySelectorAll('li:not(:first-child)');
+const paragraphItems = document.querySelectorAll('p:not(:first-child');
 
 listItems.forEach(item => {
     const images = item.querySelectorAll('img');
@@ -6,3 +7,10 @@ listItems.forEach(item => {
         img.loading = 'lazy';
     });
 });
+
+paragraphItems.forEach(item => {
+    const images = item.querySelectorAll('img');
+    images.forEach(img => {
+        img.loading = 'lazy';
+    })
+})
