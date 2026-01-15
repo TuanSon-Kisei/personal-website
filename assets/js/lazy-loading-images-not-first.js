88146e667185ecba6826880761dd9c081b556a49
Tuan Son Nguyen <kisei.tuanson.work@gmail.com>
2026-01-15 09:07:15 +0700

    Not lazy loading images for project page content

--- assets/js/lazy-loading-images-not-first.js
+++ assets/js/lazy-loading-images-not-first.js
@@ -1,5 +1,4 @@
 const listItems = document.querySelectorAll('li:not(:first-child)');
-const paragraphItems = document.querySelectorAll('p:not(:first-child');
 
 listItems.forEach(item => {
     const images = item.querySelectorAll('img');
@@ -7,10 +6,3 @@ listItems.forEach(item => {
         img.loading = 'lazy';
     });
 });
-
-paragraphItems.forEach(item => {
-    const images = item.querySelectorAll('img');
-    images.forEach(img => {
-        img.loading = 'lazy';
-    })
-})
