6d61214a37e3e82359bd8cd34bf909ddfd03748c
Tuan Son Nguyen <kisei.tuanson.work@gmail.com>
2026-05-08 11:26:49 +0700

    Used GSAP for Swup transition

--- assets/js/page-transition.js
+++ assets/js/page-transition.js
@@ -1,3 +1,30 @@
 import Swup from "swup";
-const swup = new Swup();
+import gsap from "gsap";
+
+const swup = new Swup({
+  animationSelector: false,
+});
+
+swup.hooks.replace("animation:out:await", async () => {
+  await gsap.to("#swup", {
+    opacity: 0,
+    y: -20,
+    duration: 0.4,
+    ease: "power2.inOut",
+  });
+});
+
+swup.hooks.replace("animation:in:await", async () => {
+  await gsap.fromTo(
+    "#swup",
+    { opacity: 0, y: 20 },
+    {
+      opacity: 1,
+      y: 0,
+      duration: 0.4,
+      ease: "power2.out",
+    },
+  );
+});
+
 export default swup;
