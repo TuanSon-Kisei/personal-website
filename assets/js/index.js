08b2f6afc5c427cd2d6f4ab1a70af9b6a6df763f
Tuan Son Nguyen <kisei.tuanson.work@gmail.com>
2026-06-15 23:37:53 +0700

    Added visited attribute for link text and uploaded new page

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,2 @@
 import "./lazy-loading-images-not-first.js";
 import "./toggle-dark-mode.js";
-import "./clickable-reading-card.js";
--- content/readings/_index.md
+++
@@ -1,3 +0,0 @@
----
-title: "Readings - Kisei's personal space"
----
